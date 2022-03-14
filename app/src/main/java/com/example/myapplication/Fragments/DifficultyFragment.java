72115bc2ef9b7ffd9b7aba37a5a1f6cd024e76b8
Valentin <valentinpfeiffer@web.de>
2022-03-14 13:58:55 +0100

    Refactoring

--- app/src/main/java/com/example/myapplication/Fragments/DifficultyFragment.java
+++ app/src/main/java/com/example/myapplication/Fragments/DifficultyFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -57,7 +56,6 @@ public class DifficultyFragment extends Fragment {
     }
 
     public void createFilm(){
-        TextView text = requireView().findViewById(R.id.textView2);
         //vM = new ViewModelProvider(requireActivity()).get(SelectedFilmViewModel.class);
         switch (vM.getSelectedFilm()){
             case 1:
