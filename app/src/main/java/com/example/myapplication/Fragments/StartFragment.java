58d05bc6e919ed2e52b2b4242b4e163a064401eb
Valentin <valentinpfeiffer@web.de>
2022-03-20 20:50:26 +0100

    Coloring

--- app/src/main/java/com/example/myapplication/Fragments/StartFragment.java
+++ app/src/main/java/com/example/myapplication/Fragments/StartFragment.java
@@ -1,6 +1,5 @@
 package com.example.myapplication.Fragments;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,18 +14,9 @@ import com.example.myapplication.R;
 public class StartFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         requireActivity().setTitle(R.string.first_fragment_label);
 
-        //System Ui Dark Mode
-        switch (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
-            case Configuration.UI_MODE_NIGHT_YES:
-                System.out.println("DARK");
-                break;
-            case Configuration.UI_MODE_NIGHT_NO:
-                System.out.println("LIGHT");
-                break;
-        }
         return inflater.inflate(R.layout.fragment_start, container, false);
     }
 
